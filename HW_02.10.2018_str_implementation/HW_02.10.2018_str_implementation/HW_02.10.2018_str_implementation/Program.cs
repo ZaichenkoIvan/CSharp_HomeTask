5e28db7899485899321ad3f883e9193a517ab343
Ivan Zaichenko <i.v.zaichenko70@gmail.com>
2018-10-03 13:17:29 +0300

    HomeTask 02 10 2018 implemantation

---
+++ HW_02.10.2018_str_implementation/HW_02.10.2018_str_implementation/HW_02.10.2018_str_implementation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HW_02._10._2018_str_implementation
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string str = "implementation";
+            str = str.ToUpper();
+            char[] strChars = str.ToCharArray();
+            for (int i = 0; i < strChars.Length; i++)
+            {
+                if (strChars[i] == 'M')
+                    strChars[i] = 'm';
+            }
+
+            foreach (char letter in strChars)
+            {
+                Console.Write(letter);
+            }
+        }
+    }
+}
+
